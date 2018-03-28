e1d1ef97feda175520ebde7f374caa5b6b73c57d
Edward Nunez <Edward.Nunez@cyberark.com>
2018-03-28 11:45:50 -0700

    pdk converted

--- spec/classes/cyberark_aim_package_spec.rb
+++ spec/classes/cyberark_aim_package_spec.rb
@@ -1,19 +1,15 @@
 require 'spec_helper'
 
-describe 'cyberark_aim::package' do 
-    
-    context 'AIM already installed' do
-        
-        let(:facts) {
-             { installed_carkaim: "CARKaim-9.80-0.85.x86_64" } 
-        }
-        
-        let(:params) {
-            { cyberark_aim__ensure => 'present'  }
-        }
-        
-        it { is_expected.to contain_package('CARKaim').with_ensure('installed') }
-            
-        
+describe 'cyberark_aim::package' do
+  context 'AIM already installed' do
+    let(:facts) do
+      { installed_carkaim: 'CARKaim-9.80-0.85.x86_64' }
     end
+
+    let(:params) do
+      { cyberark_aim__ensure => 'present' }
+    end
+
+    it { is_expected.to contain_package('CARKaim').with_ensure('installed') }
+  end
 end
